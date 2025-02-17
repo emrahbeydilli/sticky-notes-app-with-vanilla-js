fd65d24703c39b4a18bb56e047d4cf7b5790e04a
Emrah Beydilli <emrahbeydilli@gmail.com>
2025-02-17 06:47:12 +0300

    adding UI

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const getNotes = () => {
+    
+}
+
+const saveNotes = () => {
+    
+}
+
+const createNoteElement = (id,content) => {
+    
+}
+
+const addNote = () => {
+    
+}
+
+const updateNote = (id,newContent) => {
+    
+}
+
+const deleteNote = (id,element) => {
+    
+}
